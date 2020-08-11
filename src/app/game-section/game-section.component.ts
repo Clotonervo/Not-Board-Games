12c4d5b77deb3652d3479a09d19eec73b39bc3dc
Clotonervo <sam.hopkins@blastsoccer.net>
2020-08-10 19:23:00 -0600

    Some styling changes

--- src/app/game-section/game-section.component.ts
+++ src/app/game-section/game-section.component.ts
@@ -12,16 +12,10 @@ export class GameSectionComponent implements OnInit {
   @Input() type: string;
   @Input() description: string;
   @Input() imagePath: string;
+  @Input() price: string;
   
   constructor() { }
 
-  ngOnInit(): void {
-    // console.log(this.title)
-    // console.log(this.players)
-    // console.log(this.type)
-    // console.log(this.description)
-    // console.log(this.imagePath)
-
-  }
+  ngOnInit(): void {}
 
 }
