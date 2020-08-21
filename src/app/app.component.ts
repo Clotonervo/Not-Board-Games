6cb7ec68fa1ef6d60ae4157be581ddc4460c339b
Samuel R. Hopkins <srhopkins@mastercontrol.com>
2020-08-21 15:02:40 -0600

    Fixed up commits and picture should scroll good

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Not-Board-Games';
   gamesArray = [
-    {title:"Agricola", players:"2-5", type:"Strategy", price:"$5", imagePath:"../assets/agricula1.png", description:"In Agricola (Latin for 'farmer'), you're a farmer in a wooden shack with your spouse and little else. On a turn, you get to take only two actions, one for you and one for the spouse, from all the possibilities you'll find on a farm: collecting clay, wood or stone; building fences; and so on. You might think about having kids in order to get more work accomplished, but first you need to expand your house. And what are you going to feed all the little rug rats? Agricola is a turn based game."},
+    {title:"Agricola", players:"2-5", type:"Strategy", price:"$5", items:["../assets/agricula1.png", "../assets/agricula2.png"], description:"In Agricola (Latin for 'farmer'), you're a farmer in a wooden shack with your spouse and little else. On a turn, you get to take only two actions, one for you and one for the spouse, from all the possibilities you'll find on a farm: collecting clay, wood or stone; building fences; and so on. You might think about having kids in order to get more work accomplished, but first you need to expand your house. And what are you going to feed all the little rug rats? Agricola is a turn based game."},
     // {title:"Agricula", players:"2-5", type:"Strategy", imagePath:"../assets/agricula1.png", description:"This is a big game"}
   ];
 }
--- src/app/game-section/game-section.component.ts
+++ src/app/game-section/game-section.component.ts
@@ -11,11 +11,42 @@ export class GameSectionComponent implements OnInit {
   @Input() players: string;
   @Input() type: string;
   @Input() description: string;
-  @Input() imagePath: string;
+  @Input() items: [any];
   @Input() price: string;
-  
+  slideIndex: number = 1;
+
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.items)
+    console.log(this.slideIndex)
+  }
+
+  ngAfterViewChecked(): void {
+    this.showSlides(this.slideIndex);
+  }
+
+  showSlides(n) {
+    var i;
+    var slides = <HTMLElement[]><any>document.getElementsByClassName("mySlides");
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[this.slideIndex-1].style.display = "block";
+  }
+
+  // Next/previous controls
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+
 
 }
