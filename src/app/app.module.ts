8ed833e108ee94671fa88bbe3aaf1d62a8f8151c
Clotonervo <sam.hopkins@blastsoccer.net>
2020-08-26 12:24:06 -0600

    More progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { GameSectionComponent } from './game-section/game-section.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GamesWrapperComponent } from './games-wrapper/games-wrapper.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameSectionComponent,
-    GamesWrapperComponent
+    GamesWrapperComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/app/game-section/game-section.component.ts
+++ src/app/game-section/game-section.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { title } from 'process';
 
 @Component({
   selector: 'app-game-section',
