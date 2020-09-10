4c537bb9a6034857ddbf6940ff90ffa6589cedee
Clotonervo <sam.hopkins@blastsoccer.net>
2020-09-09 21:08:24 -0600

    Added responsive navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Not-Board-Games';
   currentlySelectedScreen = 'About Us';
+  menuVisible = true;
 
   changePage(newPage){
     this.currentlySelectedScreen = newPage;
+  }
+
+  toggleMenu(){
+    this.menuVisible ? this.menuVisible = false : this.menuVisible = true;
   }
 }
